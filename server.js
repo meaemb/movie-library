4131c547e4c094e4d01ec9164f487d4673e8e831
meaemb <makhanbetiyarb@mail.ru>
2025-12-17 17:43:58 +0300

    Add Home and About routes and pages

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/views/about.html');
+});
+
+
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
 });
