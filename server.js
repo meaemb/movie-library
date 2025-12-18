91c3904b0609de166be29ee1842ef84888027959
meaemb <makhanbetiyarb@mail.ru>
2025-12-18 23:43:22 +0300

    Add contact page and POST route

--- server.js
+++ server.js
@@ -3,12 +3,28 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/about', (req, res) => {
   res.sendFile(__dirname + '/views/about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/views/contact.html');
+});
+
+app.post('/contact', (req, res) => {
+  console.log(req.body); 
+
+  res.send(`
+    <h2>Thanks, ${req.body.name}!</h2>
+    <p>Your message has been received.</p>
+    <a href="/contact">Go back</a>
+  `);
 });
 
 
