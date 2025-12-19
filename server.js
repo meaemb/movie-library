54b98e8f56f43506a45ee7e93c12467fbb1cf0ad
meaemb <makhanbetiyarb@mail.ru>
2025-12-19 09:07:34 +0300

    Add 404 page and navigation

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require('express');
+const path = require('path');  
 const app = express();
 
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/views/about.html');
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(__dirname + '/views/contact.html');
+  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
 app.post('/contact', (req, res) => {
   console.log(req.body); 
-
   res.send(`
     <h2>Thanks, ${req.body.name}!</h2>
     <p>Your message has been received.</p>
@@ -27,7 +26,11 @@ app.post('/contact', (req, res) => {
   `);
 });
 
+app.use((req, res) => {
+  console.log('404 triggered for:', req.url);
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
-});
+}); 
